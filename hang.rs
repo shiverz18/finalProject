7e4b6ea7dd7df75c87e5e3ad54586a9a7635b609
UVa student <student@student-VirtualBox.(none)>
2013-11-17 18:08:49 -0500

    added basic graphics

--- hang.rs
+++ hang.rs
@@ -4,6 +4,23 @@ use std::rand::Rng;
 
 
 
+fn draw(level: uint){
+	let mut myfile: ~str = level.to_str();
+	myfile = myfile + ".txt";
+	  
+	let read: Result<@Reader, ~str>;
+	read = io::file_reader(~path::Path(myfile));
+
+	if read.is_ok() {
+	let file = read.unwrap();
+	let text: ~[~str] = file.read_lines();
+        for i in range(0, text.len()) {	
+		println(text[i]);
+        }
+	}
+	
+}
+
 fn load(filename: ~str) -> ~[~str] {
 	let read: Result<@Reader, ~str>;
 	read = io::file_reader(~path::Path(filename));
@@ -35,14 +52,14 @@ fn main() {
 	      
         
 	let mut j = 0;
-        let max_guess : uint = 8;
+        let max_guess : uint = 6;
 	while (j < max_guess){
 
 		let line = io::stdin().read_line();
 		let guess_char: char = line.char_at(0);
 		println(fmt!("%? guessed: ", guess_char));
 		let has_char: bool = word.contains_char(guess_char);
-	
+	        draw(j+1);
 		if (has_char){
        			let mut i = 0;	
 			while (i < word_len) {
@@ -61,5 +78,6 @@ fn main() {
 			j += 1;
 		}
 	}
+	println("Out of guesses - you lose.");
 
 }
