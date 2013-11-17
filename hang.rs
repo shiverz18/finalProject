b39a92ca70db69f48d6164a303361297b53a10b2
UVa student <student@student-VirtualBox.(none)>
2013-11-17 15:39:46 -0500

    guess letters

--- hang.rs
+++ hang.rs
@@ -1,4 +1,4 @@
-use std::{io, run, uint, os, path, libc ,str};
+use std::{io, path};
 use std::rand;
 use std::rand::Rng;
 
@@ -20,20 +20,47 @@ fn load(filename: ~str) -> ~[~str] {
 }
 
 
-
 fn main() {
         let mut rng = rand::task_rng();
 	let dictionary: ~[~str] = load(~"words.txt");
 	let max_size: uint = dictionary.len();
         let n: uint = rng.gen_integer_range(0u, max_size);
-	let mut word: ~str = dictionary[n];
-	println(fmt!("%?", n));
-	println(fmt!("%?",max_size));	
-	println(word);        
-	let mut guesses :~[char] = "".iter().collect();
-        let line = io::stdin().read_line();
-	let guess_char: char = line.char_at(0);
-	println(fmt!("%? guessed: ", guess_char));
-	let mut has_char: bool = word.contains_char(guess_char);
-	println(fmt!("%?", has_char));	
+	let word: ~str = dictionary[n];	
+	let word_len: uint = word.char_len();
+	
+	let mut i = 0;	
+	while (i < word_len) {
+        	print("_ ");
+		i += 1;
+        }
+        println("");
+	      
+        
+	let mut j = 0;
+        let max_guess : uint = 8;
+	while (j < max_guess){
+
+		let line = io::stdin().read_line();
+		let guess_char: char = line.char_at(0);
+		println(fmt!("%? guessed: ", guess_char));
+		let has_char: bool = word.contains_char(guess_char);
+	
+		if (has_char){
+       			let mut i = 0;	
+			while (i < word_len) {
+				if(word.char_at(i) != guess_char){
+        	 			print("_ ");
+                		}else{
+		 			print(fmt!("%c", guess_char));
+		 			print(" ");
+				}
+				i += 1;
+       			 }
+		
+		}
+		else{
+			j += 1;
+		}
+	}
+
 }
