515af210f248d7f67446f85934c31861f60bb4a1
UVa student <student@student-VirtualBox.(none)>
2013-11-18 13:14:42 -0500

    fixed word and display

--- hang.rs
+++ hang.rs
@@ -1,10 +1,11 @@
-use std::{io, path, libc, os};
+use std::{io, path};
 use std::rand;
 use std::rand::Rng;
 
-
-
-fn draw(level: uint){
+fn end(level: uint, state: ~[~str], word:~str){
+	println("|H| ||  /|A\\  |N|\\ ||   /|G\\\\");	
+	println("|H|H|| /A|_\\\\ |N| \\||  |G|  __ ");
+	println("|H| || |A| || |N|  ||   \\|G/|  man ");	
 	let mut myfile: ~str = level.to_str();
 	myfile = myfile + ".txt";
 	  
@@ -16,9 +17,62 @@ fn draw(level: uint){
 	let text: ~[~str] = file.read_lines();
         for i in range(0, text.len()) {	
 		println(text[i]);
+		
         }
 	}
+	println("");
+	println("");
+
+	print("   ");	
+	for j in range(0, state.len()){
+		print(state[j]);
+		print(" ");
+	}	
+	println("");
+	println("");
+	println(fmt!("   The word was: %s",word.to_str()));
+	println("   PLAY AGAIN? ");
+}
+
+
+fn draw(level: uint, state: ~[~str], past_guess: ~[~str]){
+	println("");
+	println("|H| ||  /|A\\  |N|\\ ||   /|G\\\\");	
+	println("|H|H|| /A|_\\\\ |N| \\||  |G|  __ ");
+	println("|H| || |A| || |N|  ||   \\|G/|  man ");	
+	let mut myfile: ~str = level.to_str();
+	myfile = myfile + ".txt";
+	  
+	let read: Result<@Reader, ~str>;
+	read = io::file_reader(~path::Path(myfile));
+
+	if read.is_ok() {
+	let file = read.unwrap();
+	let text: ~[~str] = file.read_lines();
+        for i in range(0, text.len()) {	
+		println(text[i]);
+		
+        }
+	}
+	println("");
+	println("");
+
+	print("   ");	
+	for j in range(0, state.len()){
+		print(state[j]);
+		print(" ");
+	}	
+	println("");
+	println("");
 	
+	print("   Past Guesses: ");
+	for j in range(0, past_guess.len()){
+		print(past_guess[j]);
+                print(" ");
+	}
+	println("");
+	println("");
+	print("   Enter Next Guess: ");
 }
 
 fn load(filename: ~str) -> ~[~str] {
@@ -62,45 +116,60 @@ fn main() {
 	
 	//Stores the guessed word
 	let mut i = 0;	
-	let mut guessedWord: ~[~str] = ~[];
+	let mut stateWord: ~[~str] = ~[];
 	while (i < word_len) {
-        	guessedWord = guessedWord + ~[~""];
+        	stateWord = stateWord + ~[~"_"];
 		i += 1;
         }
-        println(guessedWord.to_str());
-	println(word);
+	
+        //println(fmt!("%s Guessed Word: ", stateWord.to_str()));
+	//println(word);
 
 	let mut guesses: ~[~str] = ~[];
 	      
         
 	let mut j = 0;
+	
         let max_guess : uint = 6;
-	while (j < max_guess){
-
-		let mut line = io::stdin().read_line();
-		let mut guess_char: char = line.char_at(0);
-		println(fmt!("%? guessed: ", guess_char));
-		
+	let mut has_won: bool = false;
+	while (j < max_guess  && !has_won){
+		has_won = true;
+		for i in range(0,stateWord.len()) {
+			if(stateWord[i]== ~"_"){
+				has_won = false;
+			}
+		}
+		if(has_won){
+			end(11, stateWord.clone(), word.clone());
+			break;
+		}
+		draw(j+1, stateWord.clone(), guesses.clone());
+		let line = io::stdin().read_line();
+		let guess_char: char = line.char_at(0);
+		//println(fmt!("%? guessed: ", guess_char));
 		let has_char: bool = word.contains_char(guess_char);
-	        draw(j+1);
-		if (has_char){
+		
+		if(isDuplicate(guess_char, guesses.clone())){
+			//println(fmt!("%? Pick another letter! You have already guess ", guess_char.to_str()));
+		}else if (has_char){
+			guesses.push(guess_char.to_str());
        			let mut i = 0;	
 			while (i < word_len) {
 				if(word.char_at(i) != guess_char){
-        	 			print("_ ");
+        	 			//print("_ ");
                 		}else{
-		 			print(fmt!("%c", guess_char));
-		 			print(" ");
+		 			stateWord[i] = guess_char.to_str();
 				}
 				i += 1;	
        			 }
-			println("");
+			//println("");
 		}
-		else{
-			println("nope.");			
+		else{   
+			guesses.push(guess_char.to_str());
+			//println("nope.");			
 			j += 1;
 		}
+		
 	}
-	println("Out of guesses - you lose.");
-
+	if(j >= max_guess) {end(17, stateWord.clone(), word.clone());}
 }
